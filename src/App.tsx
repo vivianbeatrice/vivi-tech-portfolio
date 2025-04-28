220515b0923c74793def467e664394f03afe6008
vivianbeatrice <vivianbea.lovelace@gmail.com>
2025-04-28 15:28:11 -0500

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>Vivi's Tech Portfolio</h1>
+        <p>Thank you for visiting my portfolio page!</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          As you can see, things are currently under construction, but I'm eager
+          for the journey ahead!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
