c1845a4b544dcd5567278b9bcada27ee3e1cd388
vivianbeatrice <vivianbea.lovelace@gmail.com>
2025-04-29 13:18:51 -0500

    Third Commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,71 @@ function App() {
           As you can see, things are currently under construction, but I'm eager
           for the journey ahead!
         </p>
+        <br></br>
+        <h2>About</h2>
+        <p>
+          My name's Vivian Lovelace, a Technical Project Manager, Full Stack
+          Software Developer, and Writer.
+        </p>
+        <p>
+          I specialize in blending an array of technical and interpersonal
+          skills to ensure all aspects of a projects are aligned towards a
+          timely and harmonious delivery.
+        </p>
+        <h3>
+          <a href="https://docs.google.com/document/d/1F6c3d-E5YYKNAx6x2BXUD0SgoWccT1brn-YERaiGt-k/edit?tab=t.0">
+            Resume
+          </a>
+        </h3>
+        <h3>
+          <a href="https://github.com/vivianbeatrice">GitHub</a>
+        </h3>
+        <br></br>
+        <h2>Skills</h2>
+        <h3>Technical</h3>
+        <li>React</li>
+        <li>JavaScript / TypeScript</li>
+        <li>Python</li>
+        <li>SQL</li>
+        {/*<li>C#</li>*/}
+        {/*<li>Go</li>*/}
+        {/*<li>Rust</li>*/}
+        <h3>Interpersonal Skills</h3>
+        <li>Project Management</li>
+        <li>Leadership</li>
+        <li>Collaboration</li>
+        <li>Emotional Intelligence</li>
+        <li>Problem Solving</li>
+        <li>Written + Verbal Communication</li>
+        <br></br>
+        <h2>Projects</h2>
+        <h4>IBM Full Stack Software Developer Capstone</h4>
+        <p>
+          I completed the IBM Full Stack Software Developer certification course
+          to increase my expertise, gaining competency in React, TypeScript,
+          JavaScript, Python, HTML, SQL, Git, GitHub, CSS, Cloud Computing,
+          Node.js, Express.js, Data Science, AI, Flask, Bootstrap, Django,
+          Databases, Docker, Kubernetes, and more.
+        </p>
+        <h4>RE Scanner</h4>
+        <p>
+          During my time as an Operations Manager, my team was flooded with an
+          influx of rote work and no additional resources to make the increase
+          in tasks manegable. Using Python, I coded the proof of concept for an
+          internal tool that connected two APIs in order to automate and label
+          work for swifter completion. Using Agile and Scrum, I was able to
+          Project Management the completion of the RE Scanner to delivery ahead
+          of deadline, which went on to save our team $50k worth of labor within
+          the first 90 days of implementation.
+        </p>
+        <h4>
+          <a href="https://vivi-iodine.itch.io/">Web Games</a>
+        </h4>
+        <p>
+          I began my software development journey by building my own web games
+          using GML, a JavaScript and C-like language, which instilled in me a
+          love for coding.
+        </p>
       </div>
     </>
   );
